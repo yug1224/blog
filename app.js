f420e90984a42a4c9a63398475063a019654b893
Yuji Yamaguchi <yuji.yamaguchi24@gmail.com>
2022-08-01 01:15:20 +0900

    update

--- app.js
+++ app.js
@@ -6,14 +6,14 @@ const format = require('date-fns/format')
 const yaml = require('js-yaml')
 
 const now = new Date()
-const id = ulid()
-const filepath = path.resolve(`./data/${format(now, 'YYYY/MM/DD/')}`)
+const id = ulid().toLowerCase()
+const filepath = path.resolve(`./data/${format(now, 'yyyy/MM/dd/')}`)
 const filename = `${id}.md`
-const frontMatter = yaml.safeDump({
+const frontMatter = yaml.dump({
   id,
   title: '',
-  create: format(now, 'YYYY-MM-DD HH:mm'),
-  modify: format(now, 'YYYY-MM-DD HH:mm'),
+  create: format(now, 'yyyy-MM-dd HH:mm'),
+  modify: format(now, 'yyyy-MM-dd HH:mm'),
   categories: [],
 })
 const data = `---\n${frontMatter}---\n<!-- more -->\n`
